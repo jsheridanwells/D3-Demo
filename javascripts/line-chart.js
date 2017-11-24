5dcc39685969134716518c517563996aed8475ed
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-11-24 15:58:52 -0600

    chart finally lookign right

--- javascripts/line-chart.js
+++ javascripts/line-chart.js
@@ -1,64 +1,54 @@
 getWeather('53213').then(data => drawChart(getTemps(data, 'temp_max')));
 
 const drawChart = (arr) => {
-  // 2. Use the margin convention practice
-  var margin = {top: 50, right: 50, bottom: 50, left: 50}
-    , width = window.innerWidth - margin.left - margin.right // Use the window's width
-    , height = window.innerHeight - margin.top - margin.bottom; // Use the window's height
 
-  // The number of datapoints
-  var n = arr.length;
+  let margin = {top: 50, right: 50, bottom: 50, left: 50},
+    width = $('#chart').width() - margin.left - margin.right,
+    height = window.innerHeight - margin.top - margin.bottom;
 
-  // 5. X scale will use the index of our data
-  var xScale = d3.scaleLinear()
-      .domain([0, n-1]) // input
-      .range([0, width]); // output
+  let n = arr.length;
 
-  // 6. Y scale will use the randomly generate number
-  var yScale = d3.scaleLinear()
-      .domain([0, d3.max(arr) + 10]) // input
-      .range([height, 0]); // output
+  let xScale = d3.scaleLinear()
+      .domain([0, n-1])
+      .range([0, width]);
 
-  // 7. d3's line generator
-  var line = d3.line()
-      .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
-      .y(function(d) { return yScale(d.y); }); // set the y values for the line generator
+  let yScale = d3.scaleLinear()
+      .domain([0, d3.max(arr) + 10])
+      .range([height, 0]);
 
-  // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
-  var dataset = arr.map(d => {
+  let line = d3.line()
+      .x(function(d, i) { return xScale(i); })
+      .y(function(d) { return yScale(d.y); });
+
+  let dataset = arr.map(d => {
     return {'y' : d};
   });
 
-  // 1. Add the SVG to the page and employ #2
-  var svg = d3.select("#chart").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  let svg = d3.select('#chart').append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-  // 3. Call the x axis in a group tag
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+  svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(xScale));
 
-  // 4. Call the y axis in a group tag
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+  svg.append('g')
+      .attr('class', 'y axis')
+      .call(d3.axisLeft(yScale));
 
-  // 9. Append the path, bind the data, and call the line generator
-  svg.append("path")
-      .datum(dataset) // 10. Binds data to the line
-      .attr("class", "line") // Assign a class for styling
-      .attr("d", line); // 11. Calls the line generator
+  svg.append('path')
+      .datum(dataset)
+      .attr('class', 'line')
+      .attr('d', line);
 
-  // 12. Appends a circle for each datapoint
-  svg.selectAll(".dot")
+  svg.selectAll('.dot')
       .data(dataset)
-    .enter().append("circle") // Uses the enter().append() method
-      .attr("class", "dot") // Assign a class for styling
-      .attr("cx", function(d, i) { return xScale(i) })
-      .attr("cy", function(d) { return yScale(d.y) })
-      .attr("r", 5);
+      .enter().append('circle')
+      .attr('class', 'dot')
+      .attr('cx', function(d, i) { return xScale(i) })
+      .attr('cy', function(d) { return yScale(d.y) })
+      .attr('r', 5);
 }
