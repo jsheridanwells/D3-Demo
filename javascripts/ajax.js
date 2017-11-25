f606df73d24b070477ffe902b44b698f7922bed3
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-11-24 21:35:24 -0600

    fixed up interface

--- javascripts/ajax.js
+++ javascripts/ajax.js
@@ -9,3 +9,15 @@ const getWeather = (zip) => {
     .catch(error => reject(error.responseJSON.message));
   });
 };
+
+// in case api fails,
+// read from local json
+// const getWeather = () => {
+//   return new Promise((resolve, reject) => {
+//     $.ajax({
+//       url: '../data/sample.json'
+//     })
+//     .then(data => resolve(data))
+//     .catch(error => reject(error.responseJSON.message));
+//   });
+// };
